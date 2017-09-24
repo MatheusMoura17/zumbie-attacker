28029646fb27e31cce1c000ba077f6c8076abd7b
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-24 03:49:24 -0300

    feat: Pooled Bullet Spawner criado

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	private List<GameObject> bullets=new List<GameObject>();
+	
+	public void Spawn(){
+		foreach (GameObject gm in bullets) {
+			if (!gm.activeSelf) {
+				gm.transform.position = transform.position;
+				gm.transform.eulerAngles = transform.eulerAngles;
+				gm.SetActive (true);
+				return;
+			}
+		}
+
+		GameObject tempGameObject =Instantiate(bulletPrefab,transform.position,transform.rotation);
+		bullets.Add (tempGameObject);
+	}
+}
