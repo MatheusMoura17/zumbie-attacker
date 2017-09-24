dd40a3baa7b2b599ce89ca4662498027595a4686
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-24 03:29:29 -0300

    feat: Bullet programada

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 10;
+	public float lifeTime=1;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Disable", lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (speed*Time.deltaTime,0,0);
+	}
+
+	void Disable(){
+		gameObject.SetActive (false);
+	}
+
+}
