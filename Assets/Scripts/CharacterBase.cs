9c9eebd20a0cc0e36fbf733474a602246a841bd5
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-24 06:55:20 -0300

    feat: CharacterBase e EnemyBehaviour organizados em regiões

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -4,49 +4,52 @@ using UnityEngine;
 
 public abstract class CharacterBase : MonoBehaviour {
 
+	#region Vars
 	[Header("Movement")]
 	public float moveSpeed=6;
 	public float gravity=20;
 	public float jumpSpeed=8;
 	private Vector3 moveDirection=Vector3.zero;
+
+	/// <summary>
+	/// The character is running.
+	/// </summary>
 	private bool running;
+	/// <summary>
+	/// The character is attacking.
+	/// </summary>
 	private bool attacking;
+	/// <summary>
+	/// The character is killed.
+	/// </summary>
 	public bool killed;
 
-	[Header("Weapon")]
+	[Header("Components")]
 	public GameObject muzzleEffect;
-
 	public Transform lifeBar;
-
-	[Header("Tools")]
-	public int life=100;
 	public Animator myAnimator;
-	public float attackRatio=0.2f;
-	private float attackTimer;
 	public CharacterController characterController;
-	public float timeToDisable=2;
 
+	[Header("Other Tools")]
+	public int life=100;
+	public float attackRatio=0.2f;
+	public float timeToDisable=2;
+	private float attackTimer;
+	#endregion
+
+	#region Voids to childreen class 
 	protected abstract void OnShoot();
 	protected abstract void OnHitEnter(int damage);
 	protected abstract void CalculateInputs ();
 	protected abstract void OnStart ();
+	#endregion
 
-	void Start(){
-		UpdateLifeBar ();
-		OnStart ();
-	}
+	#region Life and Attack functions
 
-	void Update(){
-		CalculateInputs ();
-		if (attackTimer <= Time.time) {
-			attacking = false;
-			muzzleEffect.SetActive (false);
-		}
-		moveDirection.y -= gravity*Time.deltaTime;
-		characterController.Move (moveDirection*Time.deltaTime);
-		ApplyAnimations ();
-	}
-		
+	/// <summary>
+	/// Called when the player takes damage. Determines if the player died
+	/// </summary>
+	/// <param name="damage">Damage.</param>
 	public void Hit(int damage){
 		life -= damage;
 		if (life <= 0) {
@@ -58,23 +61,25 @@ public abstract class CharacterBase : MonoBehaviour {
 		OnHitEnter (damage);
 	}
 
+	/// <summary>
+	/// Updates the life bar in scene.
+	/// </summary>
 	protected void UpdateLifeBar(){
 		Vector2 scale=lifeBar.localScale;
 		scale.x=(float)life/100f;
 		lifeBar.localScale = scale;
 	}
 
+	/// <summary>
+	/// Disable the player in scene
+	/// </summary>
 	public void Disable(){
 		gameObject.SetActive (false);
 	}
 
-	private void ApplyAnimations(){
-		myAnimator.SetBool ("jumping", !characterController.isGrounded);
-		myAnimator.SetBool ("running", running);
-		myAnimator.SetBool ("attacking", attacking);
-		myAnimator.SetBool ("dead", killed);
-	}
-
+	/// <summary>
+	/// Makes true attacking when the attackTimer hits the attackRatio
+	/// </summary>
 	public void Attack(){
 		if (attackTimer <= Time.time) {
 			attackTimer = Time.time+attackRatio;
@@ -84,11 +89,34 @@ public abstract class CharacterBase : MonoBehaviour {
 		}
 	}
 
+	#endregion
+
+	#region Animations and Sounds
+	/// <summary>
+	/// Apply base animations
+	/// </summary>
+	private void ApplyAnimations(){
+		myAnimator.SetBool ("jumping", !characterController.isGrounded);
+		myAnimator.SetBool ("running", running);
+		myAnimator.SetBool ("attacking", attacking);
+		myAnimator.SetBool ("dead", killed);
+	}
+	#endregion
+
+	#region Movementation functions
+
+	/// <summary>
+	/// Makes the player jump if on the floor
+	/// </summary>
 	public void Jump(){
 		if (characterController.isGrounded)
 			moveDirection.y = jumpSpeed;
 	}
 
+	/// <summary>
+	/// Move the character in horizontal position
+	/// </summary>
+	/// <param name="direction">Horizontal direction.</param>
 	public void Move(float direction){
 		if (direction > Constants.INPUT_ERROR_MARGIN) {
 			if (transform.eulerAngles.y != 0) {
@@ -110,7 +138,26 @@ public abstract class CharacterBase : MonoBehaviour {
 			running = false;
 			moveDirection.x = 0;
 		}
-			
+
+	}
+
+	#endregion
+
+	#region Unity functions
+	void Start(){
+		UpdateLifeBar ();
+		OnStart ();
+	}
+
+	void Update(){
+		CalculateInputs ();
+		if (attackTimer <= Time.time) {
+			attacking = false;
+			muzzleEffect.SetActive (false);
+		}
+		moveDirection.y -= gravity*Time.deltaTime;
+		characterController.Move (moveDirection*Time.deltaTime);
+		ApplyAnimations ();
 	}
 
 	void OnTriggerEnter(Collider collider){
@@ -120,5 +167,6 @@ public abstract class CharacterBase : MonoBehaviour {
 			item.Disable ();
 		}
 	}
+	#endregion
 
 }
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class EnemyBehaviour : CharacterBase
 {
 
+	#region Vars
+	[HideInInspector]
 	public GameObject[] players;
 
+	[Header("Components")]
 	public GameObject damageObject;
 	public CharacterBase currentPlayer;
 
+	[Header("IA Tools")]
 	private int targetPlayer = -1;
 	public float distanceToFollow = 5;
 	public float distanceToAttack = 1;
 	public float lookOutWaitTime=2;
 	private float waitCounter = 0;
-
 	private bool wait;
+	#endregion
+
+	#region CharacterBase inherited functions
 
 	protected override void OnStart ()
 	{
 		players = GameObject.FindGameObjectsWithTag (Constants.PLAYER_TAG);
 	}
-
+		
 	protected override void CalculateInputs ()
 	{
 		if (killed) {
@@ -68,19 +74,6 @@ public class EnemyBehaviour : CharacterBase
 		}
 	}
 
-	private float GetPlayerDirection (int playerIndex)
-	{
-		return transform.position.x - players [playerIndex].transform.position.x > 0 ? -1 : 1;
-	}
-
-	private int GetPlayerIndexAproximated ()
-	{
-		for (int i = 0; i < players.Length; i++)
-			if (players [i].activeSelf && Vector2.Distance (players [i].transform.position, transform.position) <= distanceToFollow)
-				return i;
-		return -1;
-	}
-
 	protected override void OnShoot ()
 	{
 		damageObject.SetActive (true);
@@ -89,5 +82,32 @@ public class EnemyBehaviour : CharacterBase
 	protected override void OnHitEnter (int damage)
 	{
 	}
+
+	#endregion
+
+	#region Player utilities
+	/// <summary>
+	/// Gets the player direction in scene
+	/// </summary>
+	/// <returns>The player direction.</returns>
+	/// <param name="playerIndex">Player index.</param>
+	private float GetPlayerDirection (int playerIndex)
+	{
+		return transform.position.x - players [playerIndex].transform.position.x > 0 ? -1 : 1;
+	}
+
+	/// <summary>
+	/// Returns the index of the closest player that is alive 
+	/// </summary>
+	/// <returns>The player index aproximated.</returns>
+	private int GetPlayerIndexAproximated ()
+	{
+		for (int i = 0; i < players.Length; i++)
+			if (players [i].activeSelf && Vector2.Distance (players [i].transform.position, transform.position) <= distanceToFollow)
+				return i;
+		return -1;
+	}
+
+	#endregion
 
 }
